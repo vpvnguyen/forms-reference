c4b15ff538c183cce0a9021fbf0f4a4a913f3030
Vincent Nguyen <vpvnguyen@gmail.com>
2021-07-07 23:01:07 -0700

    create formv2

---
+++ src/components/FormV2/FormV2.js
@@ -0,0 +1,123 @@
+import { useState, useReducer } from "react";
+
+const INIT = "INIT";
+const SHOW = "SHOW";
+const HIDE = "HIDE";
+const SAVE = "SAVE";
+const COMPLETED = "COMPLETED";
+
+const formReducer = (formState, action) => {
+  switch (action.type) {
+    case INIT:
+      return { ...formState, showForm: false, completed: false };
+    case SHOW:
+      return {
+        ...formState,
+        showForm: true,
+        data: action.payload,
+      };
+    case HIDE:
+      return {
+        ...formState,
+        showForm: false,
+        data: action.payload,
+      };
+    case SAVE:
+      return {
+        ...formState,
+        showForm: false,
+        completed: true,
+        data: action.payload,
+      };
+    case COMPLETED:
+      return { ...formState };
+    default:
+      throw new Error(
+        "formReducer error: formState, action",
+        formState,
+        action
+      );
+  }
+};
+
+const useFormReducer = () => {
+  const [formState, dispatchForm] = useReducer(formReducer, {
+    data: null,
+    showForm: false,
+    completed: false,
+  });
+
+  return [formState, dispatchForm];
+};
+
+const Form1 = () => {
+  const [formData, setFormData] = useState({ firstName: "", lastName: "" });
+  const [formState, dispatchForm] = useFormReducer();
+
+  const cancel = () => {
+    dispatchForm({ type: HIDE });
+  };
+
+  const save = (e) => {
+    e.preventDefault();
+    dispatchForm({ type: SAVE, payload: formData });
+    console.log("formState.data", formState.data);
+  };
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form action="">
+      <h1>Form1</h1>
+      <input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleOnChange}
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleOnChange}
+      />
+      <button onClick={cancel}>Cancel</button>
+      <button onClick={save}>Save</button>
+    </form>
+  );
+};
+
+const FormComponent = ({ form }) => {
+  const [formState, dispatchForm] = useFormReducer();
+
+  const addForm = () => {
+    dispatchForm({ type: SHOW });
+  };
+
+  return (
+    <div>
+      <button onClick={addForm}>Add</button>
+      {formState.showForm && <div>{form}</div>}
+    </div>
+  );
+};
+
+export const FormV2 = () => {
+  return (
+    <div>
+      <div
+        style={{
+          margin: "40px",
+          backgroundColor: "lightgrey",
+          width: "500px",
+        }}
+      >
+        <FormComponent form={<Form1 />} />
+      </div>
+    </div>
+  );
+};
