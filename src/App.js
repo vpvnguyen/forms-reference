e195db86d30f55355df7415606de817da63f6b8a
Vincent Nguyen <vpvnguyen@gmail.com>
2021-07-07 23:00:39 -0700

    use formv2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { FormV1 } from "./components/FormV1/FormV1";
+import { FormV2 } from "./components/FormV2/FormV2";
 
 const App = () => {
   return (
     <div className="app">
-      <FormV1 />
+      {/* <FormV1 /> */}
+      <FormV2 />
     </div>
   );
 };
